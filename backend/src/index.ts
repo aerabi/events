7f0ef269f1fa61b005bdc5af62d4c769d63bd55d
steve192 <stefan@sterul.com>
2022-10-13 22:11:59 +0200

    Implement basic backend (#5)
    * Add basic backend functionality
    * Fetch all events
    * Create a new event
    * Connection to mongodb
    * Move code into src directory
    * Add api tests
    * Add documentation

---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+import { APIServer } from './APIServer';
+
+
+dotenv.config();
+APIServer.start();
---
+++ backend/src/model/events.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose';
+import { Model } from 'mongoose';
+
+type EventType = EventModel & mongoose.Document;
+
+export interface EventModel {
+    name: string;
+    link: string;
+    date: Date;
+    organizer: string;
+};
+
+const EventSchema = new mongoose.Schema({
+    name: String,
+    link: String,
+    date: Date,
+    organizer: String
+}, { timestamps: true });
+
+const Event: Model<EventType> = mongoose.model<EventType>('Event', EventSchema);
+export default Event;
