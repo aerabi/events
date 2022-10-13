fc05cdcd18bce15091ba3fa1dc9ca993f481e15a
steve192 <stefan@sterul.com>
2022-10-13 16:29:09 +0200

    Add basic backend functionality * Fetch all events * Create a new event * Connection to mongodb

---
+++ backend/MongoConnector.ts
@@ -0,0 +1,15 @@
+import * as mongoose from 'mongoose';
+
+export class MongoConnector {
+
+
+    public static async connect() {
+        await mongoose.connect(process.env.MONGODB_URI!, {}).then(() => {
+            const indexOfA = process.env.MONGODB_URI!.indexOf('@');
+            const db = indexOfA !== -1 ?
+                process.env.MONGODB_URI!.substring(0, 10) + '!_:_!' + process.env.MONGODB_URI!.substring(indexOfA) :
+                process.env.MONGODB_URI;
+            console.log('Connected to MongoDB', db);
+        });
+    }
+}
