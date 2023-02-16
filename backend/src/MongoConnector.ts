8b0c7e7ef5cb07b3c3a7751e0e7aef86d2fe8ae1
Mohammad-Ali A'râbi <aerabi@gmx.de>
2023-02-16 10:27:41 +0100

    :hammer: Fix the MongoDB URI

--- backend/src/MongoConnector.ts
+++ backend/src/MongoConnector.ts
@@ -7,8 +7,12 @@ export class MongoConnector {
     const pass = process.env.MONGODB_PASSWORD;
     const db = process.env.MONGODB_DATABASE || 'test';
     const port = process.env.MONGODB_PORT || '27017';
+
+    // Extract the hostname part of the URI
+    const hostname = host.includes('://') ? host.split('/')[2] : host.split('/')[0];
+
     const auth = user && pass ? `${user}:${pass}@` : '';
-    const uri = `mongodb://${auth}${host}:${port}/${db}`;
+    const uri = `mongodb://${auth}${hostname}:${port}/${db}`;
     console.log('Connecting to MongoDB', uri);
     await mongoose.connect(uri, {}).then(() => {
       console.log('Connected to MongoDB', db);
